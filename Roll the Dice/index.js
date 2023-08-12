91c93e4f1b89f9597bb3b981dd062299bc3de129
Nivedh Benny <115915588+NivedhBenny@users.noreply.github.com>
2023-08-12 10:54:58 +0530

    Add files via upload

---
+++ Roll the Dice/index.js
@@ -0,0 +1,43 @@
+function rollDice(randomNumber,thisImage){
+    switch(randomNumber){
+        case 1:
+            document.querySelector(thisImage).setAttribute("src","./images/dice1.png");
+            break;
+        case 2:
+            document.querySelector(thisImage).setAttribute("src","./images/dice2.png");
+            break;
+        case 3:
+            document.querySelector(thisImage).setAttribute("src","./images/dice3.png");
+            break;
+        case 4:
+            document.querySelector(thisImage).setAttribute("src","./images/dice4.png");
+            break;
+        case 5:
+            document.querySelector(thisImage).setAttribute("src","./images/dice5.png");
+            break;
+        case 6:
+            document.querySelector(thisImage).setAttribute("src","./images/dice6.png");
+            break;
+    } 
+    return;
+}
+
+var randomNumber1 = Math.floor(Math.random() * 6 +1);
+randomNumber1;
+
+rollDice(randomNumber1,".img1");
+
+
+var randomNumber2 = Math.floor(Math.random() * 6 +1);
+randomNumber2;
+rollDice(randomNumber2,".img2");
+
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 Wins!";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!";
+}
